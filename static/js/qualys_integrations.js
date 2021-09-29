0d0f8a53246493fa81c537e2bd4148ba37d9933d
Aspect13 <ad13box@gmail.com>
2021-09-29 19:55:10 +0300

    added make default for integration

--- static/js/qualys_integrations.js
+++ static/js/qualys_integrations.js
@@ -2,63 +2,75 @@ const apiPath = '/api/v1/integrations/qualys/'
 
 const preFetch = () => {
     clearErrors()
-    $('#qualys_test_connection').addClass('updating')
-    $('#qualys_submit').addClass('updating')
+    $('#qualys_test_connection').addClass('updating').addClass('disabled')
+    $('#qualys_submit').addClass('updating').addClass('disabled')
 }
 
 const postFetch = () => {
-    $('#qualys_test_connection').removeClass('updating')
-    $('#qualys_submit').removeClass('updating')
+    $('#qualys_test_connection').removeClass('updating').removeClass('disabled')
+    $('#qualys_submit').removeClass('updating').removeClass('disabled')
 }
 
-const handleError = response => {
-    $('#qualys_test_connection').removeClass('btn-success').removeClass('btn-secondary').addClass('btn-warning')
-        response.json().then(errorData => {
-            console.log(errorData)
-            errorData.forEach(item => {
-                const errorId = `error_qualys_${item.loc[0]}`
-                console.log(errorId)
-                $(`#qualys_${item.loc[0]}`).addClass('is-invalid')
-                $(`#error_qualys_${item.loc[0]}`).text(item.msg).show()
-            })
-            // errorData[0].loc[0]
+const handleError = response => response.json().then(
+    errorData => {
+        console.log(errorData)
+        errorData.forEach(item => {
+            const errorId = `error_qualys_${item.loc[0]}`
+            console.log(errorId)
+            $(`#qualys_${item.loc[0]}`).addClass('is-invalid')
+            $(`#error_qualys_${item.loc[0]}`).text(item.msg).show()
         })
-}
+    }
+)
 
-const qualysSubmit = (data) => {
+
+const qualysCreate = () => {
+    const url = $('#qualys_url').val()
+    const login = $('#qualys_login').val()
+    const password = $('#qualys_password').val()
+    const project_id = getSelectedProjectId()
     preFetch()
     fetch(apiPath, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
+        body: JSON.stringify({url, login, password, project_id})
     }).then(response => {
         console.log(response)
         postFetch()
         if (response.ok) {
-            $('#qualys_submit').removeClass('disabled')
-            $('#qualys_test_connection').removeClass('btn-warning').addClass('btn-success').removeClass('btn-secondary')
-            data.save_action === true && $('#qualys_integration').modal('hide')
-            data.save_action === true && location.reload()
+            // $('#qualys_submit').removeClass('disabled')
+            // $('#qualys_test_connection').removeClass('btn-warning').addClass('btn-success').removeClass('btn-secondary')
+            $('#qualys_integration').modal('hide')
+            location.reload()
         } else {
             handleError(response)
         }
     })
 }
 
-const qualysCreate = () => {
-    const url = $('#qualys_url').val()
-    const login = $('#qualys_login').val()
-    const password = $('#qualys_password').val()
-    // console.log(JSON.stringify({url, login, password, project_id: getSelectedProjectId(), save_action: true}))
-    qualysSubmit({url, login, password, project_id: getSelectedProjectId(), save_action: true})
-}
-
 const qualysTestConnection = () => {
     const url = $('#qualys_url').val()
     const login = $('#qualys_login').val()
     const password = $('#qualys_password').val()
-    // console.log(JSON.stringify({url, login, password}))
-    qualysSubmit({url, login, password})
+    preFetch()
+    $('#qualys_test_connection').removeClass('btn-success').addClass('btn-secondary')
+    fetch(apiPath + 'check_settings', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({url, login, password})
+    }).then(response => {
+        console.log(response)
+        postFetch()
+        if (response.ok) {
+            // $('#qualys_submit').removeClass('disabled')
+            $('#qualys_test_connection').removeClass('btn-warning').removeClass('btn-secondary').addClass('btn-success')
+            // $('#qualys_integration').modal('hide')
+            // location.reload()
+        } else {
+            $('#qualys_test_connection').removeClass('btn-success').removeClass('btn-secondary').addClass('btn-warning')
+            handleError(response)
+        }
+    })
 }
 
 const clearErrors = () => {
@@ -87,19 +99,18 @@ const qualysUpdate = (cardData) => {
     const url = $('#qualys_url').val()
     const login = $('#qualys_login').val()
     const password = $('#qualys_password').prop('readonly') ? null :$('#qualys_password').val()
-    // qualysSubmit({url, login, password, project_id: getSelectedProjectId(), save_action: true})
     preFetch()
     fetch(apiPath + cardData.id, {
         method: 'PUT',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({url, login, password})
+        body: JSON.stringify({url, login, password, is_default: true})
     }).then(response => {
         console.log(response)
         postFetch()
         if (response.ok) {
-            $('#qualys_submit').removeClass('disabled')
-            $('#qualys_test_connection').removeClass('btn-warning').addClass('btn-success').removeClass('btn-secondary')
-            $('#qualys_integration').modal('hide')
+            // $('#qualys_submit').removeClass('disabled')
+            // $('#qualys_test_connection').removeClass('btn-warning').addClass('btn-success').removeClass('btn-secondary')
+            // $('#qualys_integration').modal('hide')
             location.reload()
         } else {
             handleError(response)
@@ -123,16 +134,16 @@ const editIntegration = data => {
 $(document).ready(function () {
     $('#create_integration_qualys').on('click', () => $('#qualys_submit').on('click', qualysCreate));
     $('#qualys_test_connection').on('click', qualysTestConnection);
-    $('#qualys_url').on('change', () => $('#qualys_submit').addClass('disabled'))
-    $('#qualys_login').on('change', () => $('#qualys_submit').addClass('disabled'))
-    $('#qualys_password').on('change', () => $('#qualys_submit').addClass('disabled'))
-    // $('#qualys_close').on('click', () => {clearErrors(); clearForm()})
-    $('#qualys_integration').on('show.bs.modal', e => {
-        $('#qualys_submit').addClass('disabled')
-    })
+    // $('#qualys_url').on('change', () => $('#qualys_submit').addClass('disabled'))
+    // $('#qualys_login').on('change', () => $('#qualys_submit').addClass('disabled'))
+    // $('#qualys_password').on('change', () => $('#qualys_submit').addClass('disabled'))
+    // $('#qualys_integration').on('show.bs.modal', e => {
+    //     $('#qualys_submit').addClass('disabled')
+    // })
     $('#qualys_integration').on('hidden.bs.modal', e => {
       clearErrors();
       clearForm();
       $('#qualys_submit').prop("onclick", null).off("click");
+      console.log('clearing click')
     })
 });
