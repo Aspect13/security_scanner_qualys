0f820454553f4d78795edc1cc64f61b193700198
Aspect13 <ad13box@gmail.com>
2021-10-13 19:58:31 +0300

    fixed test params table fixed rpc for integrations that are not registered, but exist fixed type fields for test params

--- components/render_qualys.py
+++ components/render_qualys.py
@@ -1,3 +1,5 @@
+from queue import Empty
+
 from flask import render_template
 
 
@@ -22,7 +24,10 @@ def render_qualys_integration_card(context, slot, payload):
     )
 
 def render_qualys_reporter_toggle(context, slot, payload):
-    integrations = context.rpc_manager.timeout(5).integrations_get_project_integrations_by_name(payload['id'], 'qualys')
+    try:
+        integrations = context.rpc_manager.timeout(5).integrations_get_project_integrations_by_name(payload['id'], 'qualys')
+    except Empty:
+        integrations = []
     payload['project_integrations'] = integrations
     return render_template(
         'qualys_reporter_toggle.html',
