e76e6a5a8cd5794082bbab15cd220871f9477bee
Aspect13 <ad13box@gmail.com>
2021-10-20 22:36:55 +0300

    added validation to creation modal added advanced parameters for qualys integration fixed edit run test fixed modal headers

--- static/js/qualys_reporter_toggle.js
+++ static/js/qualys_reporter_toggle.js
@@ -1,16 +1,38 @@
 window['scanners_qualys'] = {
     get_data: () => {
         if ($('#integration_checkbox_qualys').prop('checked')) {
+            const id = $('#selector_qualys .selectpicker').val()
+            const option_profile_id = $('#qualys_option_profile').val()
+            const report_template_id = $('#qualys_report_template').val()
+            const scanner_types = [];
+            $('#qualys_scanner_type_internal').prop('checked') && scanner_types.push('internal')
+            $('#qualys_scanner_type_external').prop('checked') && scanner_types.push('external')
+
+            const scanner_pool = $('.qualys_scanner_pool_item input').toArray().reduce((accumulator, currentValue) => {
+                const value = $(currentValue).val()
+                value && accumulator.push(value)
+                return accumulator
+            }, [])
+
             return {
-                id: $('#selector_qualys .selectpicker').val(),
+                id,
+                option_profile_id,
+                report_template_id,
+                scanner_types,
+                scanner_pool
             }
         }
     },
     set_data: data => {
         console.log('settings data for qualys', data)
+        const { id, option_profile_id, report_template_id, scanner_types, scanner_pool } = data
         $('#integration_checkbox_qualys').prop('checked', true)
-        $('#selector_qualys .selectpicker').val(data.id).selectpicker('refresh')
+        $('#selector_qualys .selectpicker').val(id).selectpicker('refresh')
         $('#selector_qualys').collapse('show')
+        option_profile_id && $('#qualys_option_profile').val(option_profile_id)
+        report_template_id && $('#qualys_report_template').val(report_template_id)
+        scanner_types?.forEach(item => $(`#qualys_scanner_type_${item}`).prop('checked', true))
+        scanner_pool?.forEach(item => addPool(null, item))
     },
     clear_data: () => {
         const selector = $('#selector_qualys .selectpicker')
@@ -19,5 +41,31 @@ window['scanners_qualys'] = {
         )
         $('#integration_checkbox_qualys').prop('checked', false)
         $('#selector_qualys').collapse('hide')
+        $('#settings_qualys').collapse('hide')
+
+        $('#qualys_option_profile').val('')
+        $('#qualys_report_template').val('')
+        $('#qualys_scanner_type_internal').prop('checked', false)
+        $('#qualys_scanner_type_external').prop('checked', false)
+        $('.qualys_scanner_pool_item').remove()
     }
 }
+
+const addPool = (event, value='') => {
+    const row = `
+        <div class="row d-flex align-items-center qualys_scanner_pool_item">
+            <div class="flex-grow-1">
+                <input type="text" class="form-control form-control-alternative" placeholder="Key" value="${value}">
+            </div>
+            <button class="btn btn-primary btn-37" id="qualys_scanner_pool_minus" onclick="removePool(this)"><span class="fa fa-minus"></span></button>
+        </div>
+    `
+    $('#qualys_scanner_pool').append(row)
+}
+
+const removePool = el => $(el).closest('.qualys_scanner_pool_item').remove()
+
+
+$(document).ready(function () {
+    $('#qualys_scanner_pool_plus').on('click', addPool)
+})
